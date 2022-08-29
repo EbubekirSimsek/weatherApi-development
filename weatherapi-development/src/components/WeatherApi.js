031b5d283a3629bab31d6c6507104af3d4440bf5
Ebubekir Simsek <ebubekirsimsek01@hotmail.com>
2022-08-29 11:44:26 +0200

    Some changes made

--- weatherapi-development/src/components/WeatherApi.js
+++ weatherapi-development/src/components/WeatherApi.js
@@ -1,17 +1,16 @@
-import { WaterDamageTwoTone } from "@mui/icons-material";
 import axios from "axios";
 import "./WeatherApi.css";
 
 /**
  * The variable for our API url with a specific long and lat.
  */
-const urlPath =
+export const urlPath =
   "https://api.openweathermap.org/data/2.5/weather?lat=57.700054975231886&lon=11.965259019774122&appid=5cde6ed5653138cdcd61752763393ab2&units=metric";
 
 /**
  * Here we make our API GET request for the weather today.
  */
-const getTodayWeather = (url) => {
+export const getTodayWeather = (url) => {
   return (
     axios
       .get(url)
@@ -25,44 +24,3 @@ const getTodayWeather = (url) => {
       })
   );
 };
-
-/**
- * Function were we are creating the object we want.
- */
-export async function apiCallWeatherToday() {
-  let tmp = getTodayWeather(urlPath);
-  Promise.resolve(tmp).then((value) => {
-    let weatherObj = new TodayWeather(
-      value.coord.lat,
-      value.coord.lon,
-      value.main.temp,
-      value.weather[0].main,
-      value.weather[0].description,
-      "Göteborg"
-    );
-    console.log("The weather object", weatherObj);
-    console.log("The value variable", value);
-    return weatherObj;
-  });
-}
-
-/**
- * The class which contains our constructor for our object.
- */
-class TodayWeather {
-  constructor(
-    lat,
-    long,
-    temp,
-    mainDescription,
-    secondaryDescription,
-    locationName
-  ) {
-    this.lat = lat;
-    this.long = long;
-    this.temp = temp;
-    this.mainDescription = mainDescription;
-    this.secondaryDescription = secondaryDescription;
-    this.locationName = locationName;
-  }
-}
--- weatherapi-development/src/components/WeatherCard.js
+++ weatherapi-development/src/components/WeatherCard.js
@@ -1,22 +1,71 @@
 import React from "react";
 import "./WeatherCard.css";
-import { apiCallWeatherToday } from "./WeatherApi";
+import { getTodayWeather } from "./WeatherApi";
+import { urlPath } from "./WeatherApi";
 
+/**
+ * Function were we are creating the object we want.
+ */
+export async function apiCallWeatherToday() {
+  let tmp = getTodayWeather(urlPath);
+  Promise.resolve(tmp).then((value) => {
+    let weatherObj = new TodayWeather(
+      value.coord.lat,
+      value.coord.lon,
+      value.main.temp,
+      value.weather[0].main,
+      value.weather[0].description,
+      "Göteborg"
+    );
+    console.log("The temp is ", value.main.temp);
+    console.log("The object is", weatherObj);
+    return weatherObj;
+  });
+}
+
+/**
+ * The class which contains our constructor for our object.
+ */
+class TodayWeather {
+  constructor(
+    lat,
+    long,
+    temp,
+    mainDescription,
+    secondaryDescription,
+    locationName
+  ) {
+    this.lat = lat;
+    this.long = long;
+    this.temp = temp;
+    this.mainDescription = mainDescription;
+    this.secondaryDescription = secondaryDescription;
+    this.locationName = locationName;
+  }
+}
+
+/**
+ * Herer are we converting a date to a string by using the current or specified locale.
+ */
 const current = new Date();
 const date = `${current.getDate()} ${current.toLocaleString("en-us", {
   month: "long",
 })} ${current.getFullYear()}`;
 
-const weatherObject = apiCallWeatherToday();
+const tmp = apiCallWeatherToday();
+const temp = `${tmp.temp}`;
+const location = tmp.locationName;
 
 export default function WeatherCard() {
   return (
-    <div className="card">
-      <div className="city"> Test</div>
-      <div className="date">{date} </div>
-      <div className="weather">
-        <div className="sun">
-          <div className="temp"> 28°C</div>
+    <div>
+      <div className="card">
+        <div className="city"> Location:{location}</div>
+        <div className="date">{date} </div>
+        <div className="weather">
+          <div className="sun">
+            <div className="temp"> {temp} °C</div>
+          </div>
         </div>
       </div>
     </div>
